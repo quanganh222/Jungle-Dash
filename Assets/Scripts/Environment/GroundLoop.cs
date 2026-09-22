8a1aeda19d00e6f80b03c0497edc553c3735fdce
quanganh222 <anh006348@gmail.com>
2026-09-22 22:10:59 +0700

    sua lan 1

--- Assets/Scripts/Environment/GroundLoop.cs
+++ Assets/Scripts/Environment/GroundLoop.cs
@@ -2,19 +2,40 @@ using UnityEngine;
 
 public class GroundLoop : MonoBehaviour
 {
+    [SerializeField] private Transform ground1;
+    [SerializeField] private Transform ground2;
     [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float groundWidth = 20f;
 
-    void Update()
+    private float groundWidth;
+
+    private void Start()
+    {
+        groundWidth = Mathf.Abs(ground2.position.x - ground1.position.x);
+    }
+
+    private void Update()
     {
         // để mặt dất di chuyển sang bên trái 
-        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
-        
-        // Nếu mặt đất đã chạy hết sang bên  trái
-        if (transform.position.x <= -groundWidth)
+        ground1.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        ground2.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+
+        if (ground1.position.x <= -groundWidth)
         {
             // đưa nó sang phía bên phải 
-            transform.position += Vector3.right * groundWidth * 2f; 
+            ground1.position = new Vector3(
+                ground2.position.x + groundWidth,
+                ground1.position.y,
+                ground1.position.z
+            );
+        }
+         if (ground2.position.x <= -groundWidth)
+        {
+            // đưa nó sang phía bên phải 
+            ground2.position = new Vector3(
+                ground1.position.x + groundWidth,
+                ground2.position.y,
+                ground2.position.z
+            );
         }
     }
 }
