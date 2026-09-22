b7dca0710d6d239ddc8634d65dbc3220217c498c
quanganh222 <anh006348@gmail.com>
2026-09-22 20:56:38 +0700

    them groundloop

--- Assets/Scripts/Environment/GroundLoop.cs
+++ Assets/Scripts/Environment/GroundLoop.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class GroundLoop : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float groundWidth = 20f;
 
-    // Update is called once per frame
     void Update()
     {
+        // để mặt dất di chuyển sang bên trái 
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
         
+        // Nếu mặt đất đã chạy hết sang bên  trái
+        if (transform.position.x <= -groundWidth)
+        {
+            // đưa nó sang phía bên phải 
+            transform.position += Vector3.right * groundWidth * 2f; 
+        }
     }
 }
